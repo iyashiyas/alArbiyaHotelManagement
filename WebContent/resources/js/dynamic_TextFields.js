89ba7e5957e2a1999e2a529b2fb13d5c690acd76
msuganthan <suganthanchandrika@gmail.com>
2016-11-01 19:51:56 +0530

    Bug fixes

--- WebContent/resources/js/dynamic_TextFields.js
+++ WebContent/resources/js/dynamic_TextFields.js
@@ -1,48 +1,53 @@
 $(function() {
 	var unitLangugaeCount = 0;
-    $(document).on('click', '.btn-add', function(e) {
-    	unitLangugaeCount++
-    	e.preventDefault();
-          
-        var controlForm = $('.controls:first'),
-            currentEntry = $(this).parents('.entry:first'),
-            newEntry = $(currentEntry.clone()).appendTo(controlForm);
-           
-        newEntry.find('input').val('');
-        newEntry.find('input').attr('name', "unitLanguages["+unitLangugaeCount+"].unitLanguageName")
-        newEntry.find('select').attr('name', "unitLanguages["+unitLangugaeCount+"].id")
-        controlForm.find('.entry:not(:last) .btn-add')
-            .removeClass('btn-add').addClass('btn-remove')
-            .removeClass('btn-success').addClass('btn-danger')
-            .html('<span class="glyphicon glyphicon-minus"></span>');
-           
-    }).on('click', '.btn-remove', function(e){
+	$(document).on('click','.btn-add',function(e) {
+				unitLangugaeCount++
+				e.preventDefault();
+
+				var controlForm = $('.controls:first'), currentEntry = $(this)
+						.parents('.entry:first'), newEntry = $(
+						currentEntry.clone()).appendTo(controlForm);
+
+				newEntry.find('input').val('');
+				newEntry.find('input').attr('name',"unitLanguages[" + unitLangugaeCount+ "].unitLanguageName")
+				newEntry.find('select').attr('name',"unitLanguages[" + unitLangugaeCount + "].id")
+				controlForm.find('.entry:not(:last) .btn-add')
+					.removeClass('btn-add')
+					.addClass('btn-remove')
+					.removeClass('btn-success')
+					.addClass('btn-danger')
+					.html('<span class="glyphicon glyphicon-minus"></span>');
+			}).on('click', '.btn-remove', function(e) {
 		$(this).parents('.entry:first').remove();
 		e.preventDefault();
 		return false;
-	}); 
-} );
- 
-$(function()
-		{
-		    $(document).on('click', '.edit-btn-add', function(e)
-		    {
-		        e.preventDefault();
+	});
+});
 
-		        var controlForm = $('.editControls:last'),
-		            currentEntry = $(this).parents('.editEntry:first'),
-		            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+$(function() {
+	
+	$(document).on('click','.edit-btn-add',function(e) {
+		var editUnitLangugaeCount = $('.editEntry.input-group ').length;
+		e.preventDefault();
 
-		        newEntry.find('input').val('');
-		        controlForm.find('.editEntry:not(:first) .edit-btn-add')
-		            .removeClass('edit-btn-add').addClass('btn-remove')
-		            .removeClass('btn-success').addClass('btn-danger')
-		            .html('<span class="glyphicon glyphicon-minus"></span>');
-		    }).on('click', '.btn-remove', function(e)
-		    {
-				$(this).parents('.editEntry:first').remove();
+		var controlForm = $('.editControls:last'), currentEntry = $(this)
+			.parents('.editEntry:first'), newEntry = $(
+			currentEntry.clone()).appendTo(controlForm);
 
-				e.preventDefault();
-				return false;
-			});
+			newEntry.find('input').val('');
+			newEntry.find('#editFormLanguageLanguageId').attr('name',"unitLanguages[" + editUnitLangugaeCount + "].id")
+			newEntry.find('#editFormLanguageId').remove()
+			newEntry.find('#editFormLanguageName').attr('name',"unitLanguages[" + editUnitLangugaeCount+ "].unitLanguageName")
+			controlForm.find('.editEntry:not(:first) .edit-btn-add')
+				.removeClass('edit-btn-add')
+				.addClass('btn-remove')
+				.removeClass('btn-success')
+				.addClass('btn-danger')
+				.html('<span class="glyphicon glyphicon-minus"></span>');
+			}).on('click', '.btn-remove', function(e) {
+			$(this).parents('.editEntry:first').remove();
+
+			e.preventDefault();
+			return false;
 		});
+});
--- src/main/java/org/alArbiyaHotelManagement/repository/impl/UnitRepositoryImpl.java
+++ src/main/java/org/alArbiyaHotelManagement/repository/impl/UnitRepositoryImpl.java
@@ -42,6 +42,9 @@ public class UnitRepositoryImpl implements UnitRepository{
 	public Unit editUnit(Unit unit) {
 		List<UnitLanguage> unitLanguages = new ArrayList<UnitLanguage>();
 		for(UnitLanguage unitLanguage: unit.getUnitLanguages()) {
+			TypedQuery<Language> query = this.entityManager.createQuery("SELECT lang from Language lang WHERE lang.id=:languageId", Language.class);
+			Language language = query.setParameter("languageId", unitLanguage.getId()).getSingleResult();
+			unitLanguage.setLanguage(language);
 			unitLanguage.setUnit(unit);
 			unitLanguages.add(unitLanguage);
 		}
