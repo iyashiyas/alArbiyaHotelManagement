64c380d95e53923f5490d264c17a69f5c30d335b
Aradap_Programmer <Aradap_Programmer>
2016-11-05 15:17:09 +0300

    floor js

---
+++ WebContent/resources/js/hotel/floor.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+ 
+$( "#branchId" ).change(function() {
+
+	var branchId=$(this).val();
+	alert (branchId)
+	 
+	    $.ajax({
+	        type:'GET',
+	        contentType: "application/json",
+	        url:'getAllBuilding',
+	        data:{branchId:branchId},
+	        dataType: "json",
+	        success: function(data){ 
+             alert("coming here");
+            	//here also i am getting the alertbox in jsp
+	        },
+	        error:function(xmlHttpRequest, textStatus, errorThrown){
+	            if(xmlHttpRequest.readyState=0 || xmlHttpRequest.status == 0)
+	                return;
+	        },
+	    });
+	 
+});
+});
+ 
+
+
+// If  success function Success create new select example below 
+ 
+ /*
+     success: function(data){ 
+             $("#buildings").empty();
+            $.each(data, function(index, optionData) {
+                $("#buildings").append("<option value='"
+                     + optionData.buildingId
+                     + "'>" + optionData.buildingName
+                     + "</option>");
+            });
+        });*/
+  
+   
+ 
