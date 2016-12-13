f1767d736ea5a601f2425b9004b7a239098f5bdc
msuganthan <suganthanchandrika@gmail.com>
2016-12-13 19:04:05 +0530

    fix

--- src/main/java/org/alArbiyaHotelManagement/service/UserService.java
+++ src/main/java/org/alArbiyaHotelManagement/service/UserService.java
@@ -1,9 +1,11 @@
 package org.alArbiyaHotelManagement.service;
 
+import org.alArbiyaHotelManagement.model.User;
 import org.alArbiyaHotelManagement.model.UserDetails;
 
 public interface UserService {
 	public UserDetails addUserDetails(UserDetails userDetail);
 	public UserDetails getUserDetails(String memberId);
+	public User loadUserByUsername(String username);
 	
 }
